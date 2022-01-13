78f774136ecf756a64106096054ba10c2a4073ad
priya <priya@LAPTOP-O0H7EREG>
2022-01-13 13:05:34 +0530

    comit

---
+++ java basice/src/basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class HelloWorld {
+	public static void main(String[] args ) {
+		System.out.println("Hello World");
+	}
+	int add ( int a, int b) {//this is method
+		return a+b;
+	}
+
+}
