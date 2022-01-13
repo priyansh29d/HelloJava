714eac9cb23169862b47a380dfa8712534a7b80f
priya <priya@LAPTOP-O0H7EREG>
2022-01-13 14:36:45 +0530

    initial comit

--- java basice/src/basics/Employee.java
+++ java basice/src/basics/Employee.java
@@ -1,5 +1,20 @@
 package basics;
 
 public class Employee {
+	private String dateOfBirth;
+
+
+	private String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getAge() {
+		String age = dateOfBirth + "";
+		return 18;
+	}
 
 }
--- java basice/src/basics/IncomeTax.java
+++ java basice/src/basics/IncomeTax.java
@@ -1,5 +1,11 @@
 package basics;
 
 public class IncomeTax {
+	int calculateTax(int income) {
+		return income/10;
+	}
 
+	int calculateTax(int income, int spending) {
+		return income/10 + spending/10;
+	}
 }
