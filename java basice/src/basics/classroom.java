207985c5474dfbb2688f1584ff59a59d109d6028
priya <priya@LAPTOP-O0H7EREG>
2022-01-13 15:44:24 +0530

    yes

--- java basice/src/basics/classroom.java
+++ java basice/src/basics/classroom.java
@@ -1,5 +1,29 @@
 package basics;
 
 public class classroom {
+	public static void main(string[] args ) {
+		System.out.println("students are from "+ Student.COLLEGE_NAME);
+		int agr = Student.calculateAggregate(10, 20);
+	}
+		student priyansh = new student();
+		student bstudent = new student(1,"jain")
+				Employee someEmployee = new Employee();
+String name;		
+boolean isMale;	
+float stipend;
+static String COLLEGE_NAME = "IIT";
+Student(){
+	System.out.println("priyansh:-- jain im about to construct the box");
+	void display(){
+		System.out.println("name is"+ name+" stipend is "+stipend);
+	}
+	static int calculateAggregate(int a, int b) {
+		return a+b/2;
+	}
+	static int calculateAggregate(int a, int b) {
+		return a+b/2;
+	}
+		
+	}
 
-}
+
