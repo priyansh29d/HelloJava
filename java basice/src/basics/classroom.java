aa89cd3a5ee1f2c0990e01044ed695a0add25258
priya <priya@LAPTOP-O0H7EREG>
2022-01-13 15:49:27 +0530

    final

--- java basice/src/basics/classroom.java
+++ java basice/src/basics/classroom.java
@@ -6,24 +6,27 @@ public class classroom {
 		int agr = Student.calculateAggregate(10, 20);
 	}
 		student priyansh = new student();
-		student bstudent = new student(1,"jain")
+		student bstudent = new student(1,"Newone")
 				Employee someEmployee = new Employee();
 String name;		
 boolean isMale;	
 float stipend;
-static String COLLEGE_NAME = "IIT";
+static String COLLEGE_NAME = "SRMIST";
 Student(){
 	System.out.println("priyansh:-- jain im about to construct the box");
 	void display(){
 		System.out.println("name is"+ name+" stipend is "+stipend);
 	}
+	
 	static int calculateAggregate(int a, int b) {
 		return a+b/2;
 	}
+	
 	static int calculateAggregate(int a, int b) {
 		return a+b/2;
 	}
 		
 	}
+
 
 
